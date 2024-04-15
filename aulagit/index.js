7ebd80100674346421a8c7344d304668255f5aec
JefersonKuhn <166512818+JefersonKuhn@users.noreply.github.com>
2024-04-15 09:24:35 -0300

    Revisao 1 das pastas

---
+++ aulagit/index.js
@@ -0,0 +1,38 @@
+/*console. log("hello world")
+const nome = "Jeferson Kuhn"
+const idade = 36
+console.log ("Meu nome é" ,nome,"e tenho",idade, "anos de idade")
+// teste
+const idadeNumero = 23
+const idadetexto = idadeNumero.toString()
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)
+CustomElementRegistry
+prompt ("Qual é o seu nome")
+console.log ("Qual é o seu nome")
+// teste2
+console.log("Qual é o seu nome")
+console.log("Jeferson Kuhn")
+prompt("Jeferson Kuhn")
+const idadeNumero = 36
+const idadetexto = idadetexto.toString()
+<script src="./index.js" defer ></script>*/
+// teste3
+/*const primeironome = "jeferson"
+const segundonome = "kuhn"
+const idadenumero = "36"
+const serestudante = "sim"
+console.log("o seu nome é",primeironome,"o seu sobrenome",segundonome,"minha idade",idadenumero,"é estudante",serestudante,)*/
+let nome = prompt ("Qual é o seu nome?")
+alert (`Seu nome é ${nome}`)
+
+
+
+
+
+
+
+
+
+
+
---
+++ aulagit/template_javascript/template_javascript/index.js
@@ -0,0 +1,12 @@
+let nome = prompt ("Qual é o seu nome?")
+alert (`Seu nome é ${nome}`)
+let idade = prompt ("Qual é a sua idade?")
+alert (`sua idade é ${idade}`)
+let roupa = prompt ("Sua roupa é amarela?")
+let a = "sim"
+let b = "não"
+alert (`Sua roupa é amarela - ${roupa}`)
+let carro = prompt("Você tem carro?")
+let c = "sim"
+let d = "não"
+alert (`você tem carro - ${carro}`)
