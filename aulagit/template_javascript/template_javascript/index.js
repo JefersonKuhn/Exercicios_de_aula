bd1156c38ed19b391b9872de63de0af003c983db
JefersonKuhn <166512818+JefersonKuhn@users.noreply.github.com>
2024-04-16 11:58:15 -0300

    exercicio 5

--- aulagit/template_javascript/template_javascript/index.js
+++ aulagit/template_javascript/template_javascript/index.js
@@ -1,4 +1,4 @@
-// recebeu as variáveis
+/*// recebeu as variáveis
 let nome = prompt("Digite seu nome")
 let idade = prompt("Digite sua idade")
 
@@ -28,6 +28,92 @@ console.log (typeof mensagem)
 console.log (mensagem)
 
 // Saída de valores para Front End
-alert(mensagem)
+alert(mensagem)*/
+
+
+/*// Declaração de variavel//
+let primeironumero
+let segundonumero
+
+// Leitura de dados informados pelo usuário//
+primeironumero = prompt("Digite o primeiro numero:")
+segundonumero = prompt("digite o segundo numero")
+
+// Testes de comparação//
+resultado = primeironumero === segundonumero
+
+// Mostrar resultado na tela//
+alert(resultado)
+
+//Declaração de diferente//
+resultado = primeironumero !== segundonumero
+primeironumero = prompt("Primeiro numero é diferenteS?")
+alert(resultado) 
+
+// Declaração de maior ou menor//
+resultado = primeironumero >= segundonumero
+primeironumero = prompt("Primeiro numero é maior?")
+alert(resultado)
+resultado = primeironumero <= segundonumero
+segundonumero = prompt("Segundo numero é menor?")
+alert( resultado)*/
+
+/*//Comparadores
+let a
+let b
+let c
+let resultado
+
+// Leitura de dados informados pelo usuário//
+a = true
+b = false
+c = true
+
+// Realize operação: a && b //
+resultado = a && b
+alert(resultado)
+
+// Outras operações//
+resultado = b && c
+alert(resultado)
+resultado = a && c
+alert(resultado)
+resultado = a && b && c
+alert(resultado)
+
+resultado = a || b
+alert(resultado)
+resultado = b || c
+alert(resultado)
+resultado = a || c
+alert(resultado)
+resultado = a || b || c
+alert(resultado)*/
+
+// Exercício 5 //
+let nome
+let anodenascimento
+let anoAtual
+let idade
+let maiordeidade
+let idade2050
+
+// Leitura de dados informados//
+nome = prompt("Digite seu nome:")
+anodenascimento = prompt("Digite ano de nascimento:")
+anoAtual = prompt("Digite ano atual:")
+
+//Calcular valores//
+idade = anoAtual - anodenascimento
+maiordeidade = idade >= 18
+idade2050 = 2050 - anodenascimento
+
+// Mensagen na tela//
+alert (nome)
+alert (idade)
+alert (maiordeidade)
+alert (idade2050)
+
+
 
 
